4d82c5c1f2014f6945d7fd5db3b66f503755bfa5
Mykhailo Verkhovod <michaelverkhovod@gmail.com>
2026-07-15 16:00:34 +0300

    added icon change on copylink

--- js/player.js
+++ js/player.js
@@ -46,9 +46,22 @@ function setActiveItem(item) {
 }
 
 if (copyLinkBtn) {
-  copyLinkBtn.addEventListener('click', async () => {
-	await navigator.clipboard.writeText(videoLink.href)
-})
+  const defaultCopyContent = copyLinkBtn.innerHTML;
+  let copyTimer;
+
+  copyLinkBtn.addEventListener("click", async () => {
+    await navigator.clipboard.writeText(videoLink.href);
+
+    clearTimeout(copyTimer);
+
+    copyLinkBtn.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check-icon lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
+    `;
+
+    copyTimer = setTimeout(() => {
+      copyLinkBtn.innerHTML = defaultCopyContent;
+    }, 2000);
+  });
 }
 
 export function updateActiveItem() {
