bf4f0593042eca64a8ccf1a5c3552ae9ccf6bd03
Mykhailo Verkhovod <michaelverkhovod@gmail.com>
2026-07-20 22:54:17 +0300

    added date clear, small text style fix

--- js/dateFilter.js
+++ js/dateFilter.js
@@ -1,21 +1,51 @@
-import { refresh } from "./utils.js";
+import {refresh} from './utils.js'
 
 export const dateFrom = flatpickr("#date-from", {
   dateFormat: "Y-m-d",
-  onChange() {
-    refresh();
-  },
+
   locale: {
     firstDayOfWeek: 1,
   },
+
+  onChange(_, dateStr) {
+    toggleDateState("#date-from", !!dateStr);
+    refresh();
+  }
 });
 
 export const dateTo = flatpickr("#date-to", {
   dateFormat: "Y-m-d",
-  onChange() {
-    refresh();
-  },
+
   locale: {
     firstDayOfWeek: 1,
   },
+
+  onChange(_, dateStr) {
+    toggleDateState("#date-to", !!dateStr);
+    refresh();
+  }
 });
+
+initClearButtons();
+
+function initClearButtons() {
+  document.querySelectorAll(".date-select_wrapper").forEach(wrapper => {
+    const clearBtn = wrapper.querySelector(".clear-icon");
+    const input = wrapper.querySelector("input");
+
+    clearBtn.addEventListener("click", () => {
+      input._flatpickr.clear();
+
+      wrapper.classList.remove("has-date");
+
+      refresh();
+      });
+  });
+}
+
+
+function toggleDateState(selector, hasDate) {
+  const wrapper = document.querySelector(selector).closest(".date-select_wrapper");
+
+  wrapper.classList.toggle("has-date", hasDate);
+}
